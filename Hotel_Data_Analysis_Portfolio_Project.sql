37163820fecf6e13899da1639a31b3e499f30245
Rucha Deshpande <119965384+rucha2345@users.noreply.github.com>
2024-03-05 13:32:50 -0500

    Add files via upload

---
+++ Hotel_Data_Analysis_Portfolio_Project.sql
@@ -0,0 +1,62 @@
+with hotels as (
+Select * from dbo.['2018$']
+union
+Select * from dbo.['2019$']
+union
+Select * from dbo.['2020$'])
+
+select (stays_in_week_nights+stays_in_weekend_nights)*adr as revenue from hotels
+
+
+
+with hotels as (
+Select * from dbo.['2018$']
+union
+Select * from dbo.['2019$']
+union
+Select * from dbo.['2020$'])
+select arrival_date_year,
+hotel,
+round(sum((stays_in_week_nights+stays_in_weekend_nights)*adr),2) as revenue 
+from hotels
+group by arrival_date_year, hotel
+
+
+select * from dbo.market_segment$
+
+
+
+with hotels as (
+Select * from dbo.['2018$']
+union
+Select * from dbo.['2019$']
+union
+Select * from dbo.['2020$'])
+select * from hotels
+join dbo.market_segment$
+on hotels.market_segment = market_segment$.market_segment
+
+
+
+with hotels as (
+Select * from dbo.['2018$']
+union
+Select * from dbo.['2019$']
+union
+Select * from dbo.['2020$'])
+select * from hotels
+left join dbo.market_segment$
+on hotels.market_segment = market_segment$.market_segment
+
+
+with hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$'])
+select * from hotels
+left join dbo.market_segment$
+on hotels.market_segment = market_segment$.market_segment
+left join dbo.meal_cost$
+on meal_cost$.meal = hotels.meal
